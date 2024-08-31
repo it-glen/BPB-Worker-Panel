6246c9279d5399114705369699ae8624e7ad6cb7
it-glen <73119626+it-glen@users.noreply.github.com>
2024-08-31 17:50:34 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,10 +7,11 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'ffa12094-f315-4da0-80eb-915e947e9d7d';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
-const proxyIPs= ['bpb.yousef.isegaro.com'];
+// const proxyIPs= ['bpb.yousef.isegaro.com'];
+const proxyIPs= ['hk.cf.zhetengsha.eu.org', 'sg.cf.zhetengsha.eu.org', 'jp.cf.zhetengsha.eu.org', 'us.cf.zhetengsha.eu.org'];
 
 const defaultHttpPorts = ['80', '8080', '2052', '2082', '2086', '2095', '8880'];
 const defaultHttpsPorts = ['443', '8443', '2053', '2083', '2087', '2096'];
